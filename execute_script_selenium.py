c32dff3ee57916d610bd49a5a420d1eda7970c1c
Denis <71184288+Unp1ugged@users.noreply.github.com>
2021-09-11 18:19:43 +0300

    added file

---
+++ execute_script_selenium.py
@@ -0,0 +1,12 @@
+from os import X_OK
+from selenium import webdriver
+import math
+browser = webdriver.Chrome()
+browser.get("http://suninjuly.github.io/execute_script.html")
+x = browser.find_element_by_id("input_value").text
+solve = str(math.log(abs(12*math.sin(int(x)))))
+browser.execute_script("window.scrollBy(0, 150);")
+browser.find_element_by_id("answer").send_keys(solve)
+browser.find_element_by_class_name("form-check-label").click()
+browser.find_element_by_id("robotsRule").click()
+browser.find_element_by_tag_name("button").click()
